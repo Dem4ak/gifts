b668eec7bad871dec3973d942a5bcde0deaa9609
dem4ak <rDem4ak@gmail.com>
2017-11-04 21:27:56 +0200

    Style pages

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,5 @@ from pages.models import Page
 
 def page(request, slug):
     page = Page.objects.get(slug=slug)
-    child_pages = Page.objects.filter(parent=page.id)
+    child_pages = Page.objects.filter(parent=page.id).order_by('order')
     return render(request, 'pages/page.html', locals())
